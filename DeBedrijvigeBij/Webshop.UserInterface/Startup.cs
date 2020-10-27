d8bbc2abdeac89dfa149b7f7ff385f681c108a0c
Wesley Dierckx <dierckxwesley@outlook.com>
2020-10-27 20:23:45 +0100

    Added Connectionstring (local DB) Added Service SQL Init migrat.

--- DeBedrijvigeBij/Webshop.UserInterface/Startup.cs
+++ DeBedrijvigeBij/Webshop.UserInterface/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop.Data;
 
 namespace Webshop.UserInterface
 {
@@ -24,6 +26,13 @@ namespace Webshop.UserInterface
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            //reference shop application
+            // Assembly not clear, so added it 
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+  
+         
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
