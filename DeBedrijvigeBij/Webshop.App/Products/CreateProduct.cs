9183ca2a32241b387ded1ae08e9142c046abd632
Wesley Dierckx <dierckxwesley@outlook.com>
2020-10-31 19:43:36 +0100

    Added: functionality Create Product

--- DeBedrijvigeBij/Webshop.App/Products/CreateProduct.cs
+++ DeBedrijvigeBij/Webshop.App/Products/CreateProduct.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Webshop.App.ViewModels;
 using Webshop.Domain.Models;
 
 namespace Webshop.App.Products
@@ -16,15 +18,21 @@ namespace Webshop.App.Products
             _DbContext = DbContext;
         }
 
-        public void Do(string name, string description, decimal price)
-        {
-            _DbContext.Products.Add(new Product
-            {
-                Name = name,
-                Description=description,
-                Price=price
-            });
+        //public void Do(string name, string description, decimal price)
+        //{
+        //    _DbContext.Products.Add(new Product
+        //    {
+        //        Name = name,
+        //        Description=description,
+        //        Price=price
+        //    });
            
+        //}
+
+        public async Task Add(Product product) // task<int> bv, of task == gewoon void
+        {
+            _DbContext.Products.Add(product);
+           await _DbContext.SaveChangesAsync();
         }
     }
 }
--- DeBedrijvigeBij/Webshop.UserInterface/Controllers/ProductController.cs
+++ DeBedrijvigeBij/Webshop.UserInterface/Controllers/ProductController.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Webshop.App.Products;
+using Webshop.App.ViewModels; // proper of niet?
+using Webshop.Domain.Models;
 
 namespace Webshop.UserInterface.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+
+        private ApplicationDbContext _dbContext { get; set; }
+
+        public ProductController(ApplicationDbContext dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateProduct([Bind("Name,Description,Price")] Product product)
+        {
+            await new CreateProduct(_dbContext).Add(product);
             return View();
         }
     }
