02a29c58284922942f039b96aee70ba7683c2abe
Wesley Dierckx <dierckxwesley@outlook.com>
2020-11-01 17:18:15 +0100

    Updated ReadProduct Admin

---
+++ DeBedrijvigeBij/Webshop.App/ProductsAdmin/ReadProduct.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Webshop.App.ViewModels;
+using Webshop.Domain.Models;
+
+namespace Webshop.App.ProductsAdmin
+{
+    class ReadProduct
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ReadProduct(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public ProductAdminViewModel Get(int productId) => 
+            _dbContext.Products.Where(x => x.ProductId == productId).Select(x=> new ProductAdminViewModel
+            {
+
+            ProductId = x.ProductId,
+            Name = x.Name,
+            Description = x.Description,
+            Price = x.Price
+
+            }
+            ).FirstOrDefault();
+     
+    }
+}
